98979d905b045bbf161b818dfd75098b7084f7b3
DEVZ BILLARIUS <82283888+devzbillarius@users.noreply.github.com>
2021-12-06 20:01:29 +0700

    Update interface.sol

--- lib/interface.sol
+++ lib/interface.sol
@@ -1,1 +1,30 @@
+// SPDX-License-Identifier:MIT
 
+pragma solidity ^0.8.9;
+
+
+interface IERC20 {
+    function totalSupply() external view returns (uint256);
+    function balanceOf(address account) external view returns (uint256);
+    function transfer(address recipient, uint256 amount) external returns (bool);
+    function allowance(address owner, address spender) external view returns (uint256);
+    function approve(address spender, uint256 amount) external returns (bool);
+    function transferFrom(address sender, address recipient, uint256 amount) external returns (bool);
+    event Transfer(address indexed from, address indexed to, uint256 value);
+    event Approval(address indexed owner,address indexed spender,uint256 value);
+
+    function decimals() external view returns (uint8);
+    function symbol() external view returns (string memory);
+    function name() external view returns (string memory);
+    function getOwner() external view returns (address);
+}
+
+abstract contract Context {
+    function _msgSender() internal view virtual returns (address) {
+        return msg.sender;
+    }
+
+    function _msgData() internal view virtual returns (bytes calldata) {
+        return msg.data;
+    }
+}
