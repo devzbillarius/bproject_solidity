a8bf24bc14768654bbdb44ac70c5a6454c505132
DEVZ BILLARIUS <82283888+devzbillarius@users.noreply.github.com>
2021-12-06 20:44:28 +0700

    Create contract.sol

---
+++ lib/contract.sol
@@ -0,0 +1,137 @@
+// SPDX-License-Identifier:MIT
+
+pragma solidity ^0.8.9;
+
+import "https://github.com/devzbillarius/bproject_solidity/blob/main/lib/safeMath.sol";
+import "https://github.com/devzbillarius/bproject_solidity/blob/main/lib/ownable.sol";
+import "https://github.com/devzbillarius/bproject_solidity/blob/main/lib/interface.sol";
+import "https://github.com/devzbillarius/bproject_solidity/blob/main/lib/context.sol";
+
+
+contract  MAKECOIN is Context, IERC20, Ownable {
+    using SafeMath for uint256;
+
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowances;
+
+    uint256 private _totalSupply;
+    uint256 private _supply;
+    uint8 private _decimals;
+    string private _symbol;
+    string private _name;
+
+    constructor(string memory name_, string memory symbol_, uint8 decimals_, uint256 supply_) {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+        _supply = supply_;
+        _totalSupply = _supply * 10**_decimals;
+        _balances[msg.sender] = _supply *(10**_decimals);
+
+        emit Transfer(address(0), msg.sender, _balances[msg.sender]);
+    }
+
+    function getOwner() public view returns (address) {
+        return owner();
+    }
+
+    function decimals() public view returns (uint8) {
+        return _decimals;
+    }
+
+    function symbol() public view returns (string memory) {
+        return _symbol;
+    }
+
+    function name() public view returns (string memory) {
+        return _name;
+    }
+
+    function totalSupply() public view returns (uint256) {
+        return _totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return _balances[account];
+    }
+
+    function transfer(address recipient, uint256 amount) public returns (bool){
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    function allowance(address owner, address spender) public view returns (uint256){
+        return _allowances[owner][spender];
+    }
+
+    function approve(address spender, uint256 amount) public returns (bool){
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    function transferFrom(address sender, address recipient, uint256 amount) public returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender,_msgSender(),_allowances[sender][_msgSender()].sub(amount,"POIS: transfer amount exceeds allowance"));
+        return true;
+    }
+
+    function increaseAllowance(address spender, uint256 addedValue) public returns (bool){
+        _approve(_msgSender(),spender,_allowances[_msgSender()][spender].add(addedValue));
+        return true;
+    }
+
+    function decreaseAllowance(address spender, uint256 subtractedValue) public returns (bool){
+        _approve(_msgSender(),spender,_allowances[_msgSender()][spender].sub(subtractedValue,"POIS: decreased allowance below zero"));
+        return true;
+    }
+
+    function mint(uint256 amount) public onlyOwner returns (bool) {
+        _mint(_msgSender(), amount);
+        return true;
+    }
+
+    function burn(uint256 amount) public returns (bool) {
+        _burn(_msgSender(), amount);
+        return true;
+    }
+
+    function _transfer(address from, address to, uint256 amount) private {
+        require(from != address(0), "POIS: transfer from the zero address");
+        require(to != address(0), "POIS: transfer to the zero address");
+        require(amount > 0, "POIS: Amount must be greater than zero");
+
+        _balances[from] = _balances[from].sub(amount);
+        _balances[to] = _balances[to].add(amount);
+         
+        emit Transfer(from, to, amount);
+    }
+
+    function _mint(address account, uint256 amount) private {
+        require(account != address(0), "POIS: mint to the zero address");
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    function _burn(address account, uint256 amount) private {
+        require(account != address(0), "POIS: burn to the zero address");
+
+        _balances[account] = _balances[account].sub(amount);
+        _totalSupply = _totalSupply.sub(amount);
+        emit Transfer(account, address(0), amount);
+    }
+
+    function _approve(address owner, address spender, uint256 amount) private {
+        require(owner != address(0), "POIS: transfer from the zero address");
+        require(spender != address(0), "POIS: transfer to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    function _burnFrom(address account, uint256 amount) private {
+        _burn(account, amount);
+        _approve(account, _msgSender(), _allowances[account][_msgSender()].sub(amount));
+    }
+}
