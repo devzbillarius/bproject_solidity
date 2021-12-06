ed7260ca5040414859b02e6f5480af93a8162727
DEVZ BILLARIUS <82283888+devzbillarius@users.noreply.github.com>
2021-12-06 20:28:46 +0700

    Update interface.sol

--- lib/interface.sol
+++ lib/interface.sol
@@ -18,12 +18,3 @@ interface IERC20 {
     function getOwner() external view returns (address);
 }
 
-abstract contract Context {
-    function _msgSender() internal view virtual returns (address) {
-        return msg.sender;
-    }
-
-    function _msgData() internal view virtual returns (bytes calldata) {
-        return msg.data;
-    }
-}
